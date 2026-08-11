9bffc05cafd6ce9964847475868644abbbbd9136
Halima <halimajahanchowdhury@gmail.com>
2026-08-10 21:17:26 -0600

    Save all project changes

--- backend/collaborations/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CollaborationsConfig(AppConfig):
-    name = 'collaborations'
--- backend/study_sessions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudySessionsConfig(AppConfig):
-    name = 'study_sessions'
